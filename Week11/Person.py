4cd8809bf1453c405283425b4772990140d243f6
HootJacques <hugo.jacques@georgebrown.ca>
2025-03-21 13:21:18 -0400

    Adding person class

---
+++ Week11/Person.py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self, p_name, p_age, p_height):
+        print("Constructing Person Object")
+        self.__name = p_name
+        self.__age = p_age
+        self.__height = p_height
+        self.public__prop = "I'm a public property"
+
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name
+
+    def __del__(self):
+        print("Deleting Person Object")
+
+
+person1 = Person("Mike", 57, 187)
+
+print(f"Person Name: {person1.name}")
+person1.name = "Name Override"
+print(f"Person Name: {person1.name}")
